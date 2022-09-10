67b6fd8efe3b291fa827f245b871b9e8a870a21d
girazabal <gonzalo.irazabal.gi@gmail.com>
2022-09-10 15:19:57 -0300

    Desafio simulacro interactivo

--- main.js
+++ main.js
@@ -1,17 +1,49 @@
-//Desafio complementario 1
-//Solicitar al usuario que ingrese su nombre y el numero de veces que desea ser saludado
+//Desafio entregable simulador interactivo
 
-let nombre = prompt ('Por favor ingrese su nombre');
-
-let saludos = Number (prompt('Ingrese la cantidad de veces que desee ser saludado'));
-
-console.log(nombre)
-
-if(isNaN(saludos)) {
-    alert('Error: ingrese un numero');
+function bienvenida(){
+    alert('Bienvenido a Calabozo del cómic');
+    let usuario = prompt('Ingrese su usuario');
+    while(usuario===''){
+        usuario = prompt('Ingrese su usuario')
+    }
 }
 
-for (i = 0; i < saludos; i++){
-    alert(`Bienvenido ${nombre}`);
+function catalogoComics(){
+    let comic;
+    do {
+    comic = prompt('Ingrese el comic que desee adquirir: \n1-Ultimate spiderman #65\n2-Batman Killing joke\n3-X-men Origins\n4-The walking dead #25');
+    }while (comic!=1 && comic!=2 && comic!=3 && comic!= 4);
+    console.log(comic)
+    switch(comic){
+        case '1':
+            return 'Ultimate spiderman #65';
+        case '2':
+            return 'Batman Killing joke';
+        case '3':
+            return 'X-men Origins';
+        case '4':
+            return 'The walking dead #25';
+    }
 }
 
+function precio(comic){
+    if (comic ==='Ultimate spiderman #65'){
+    return 320;
+    }else if (comic ==='Batman Killing joke'){
+        return 450;
+    }else if(comic ==='X-men Origins'){
+        return 415;
+    }else if(comic === 'The walking dead #25'){
+        return 400;
+    }
+}
+
+function cobro(comic, precio){
+    alert('El comic seleccionado es: '+comic+'.\nEl precio a abonar es de $ '+precio+'.');
+}
+bienvenida();
+precio();
+comic = catalogoComics();
+let precioComic = precio(comic);
+cobro(comic, precioComic);
+console.log(precioComic);
