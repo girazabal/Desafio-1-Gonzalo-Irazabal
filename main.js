cf60732e81e55ef598be847f2bc186a967f0dcfd
girazabal <gonzalo.irazabal.gi@gmail.com>
2022-09-16 20:32:59 -0300

    Desafio complementario: incorporar arrays

--- main.js
+++ main.js
@@ -8,26 +8,55 @@ function bienvenida(){
 bienvenida();
 
 class Comic{
-    constructor(titulo, precio){
+    constructor(titulo, precio, id){
         this.titulo = titulo;
         this.precio = Number(precio);
         this.vendido = false;
+        this.id=id;
     }
     vender(){
         this.vendido = true;
     }
     mostrar(){
-    return this.titulo + this.precio;
+    return this.id+' '+this.titulo+' $'+this.precio+'\n';
+    }
+    textoCompra(){
+        return 'El comic seleccionado es '+this.titulo+'\nSu precio es de $ '+this.precio;
     }
 }
 const comics = [];
-const comic1 = new Comic('Ultimate spiderman #65',360);
-const comic2 = new Comic('Batman: The killing joke', 420);
-const comic3 = new Comic('X-men: Origins', 415);
-const comic4 = new Comic('The walking dead #25', 400);
+const comic1 = new Comic('Ultimate spiderman #65',360,1);
+const comic2 = new Comic('Batman: The killing joke', 420,2);
+const comic3 = new Comic('X-men: Origins', 415,3);
+const comic4 = new Comic('The walking dead #25', 400,4);
 
 comics.push(comic1, comic2, comic3, comic4);
 
+function obtenerComics() {
+    let misComics = 'Ingrese el numero de comic que desea adquirir \n';
+    for (const comic of comics) {
+        misComics = misComics + comic.mostrar();
+    }
+    return misComics;
+}
+let comicExitoso = false;
+
+ingresarComics();
+
+if(comicExitoso == false){
+    alert('No existe el comic ingresado');
+    ingresarComics();
+}
+
+function ingresarComics(){
+    let numeroIngresado=Number (prompt(obtenerComics()));
+    comics.forEach(comic => {
+        if(comic.id===numeroIngresado){
+            comicExitoso=true;
+            alert(comic.textoCompra());
+        }
+    });
+}
 
 console.log(comics);
 
